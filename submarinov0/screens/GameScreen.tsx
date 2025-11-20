599cb753f38d769eb4df345bac8449a2a4cbec78
Fulcrum <joelchicano70@gmail.com>
2025-11-20 02:06:19 +0100

    Dandole estilo al tablero

--- submarinov0/screens/GameScreen.tsx
+++ submarinov0/screens/GameScreen.tsx
@@ -16,7 +16,7 @@ const GameScreen = () => {
   const cellStyle = (cell: Cell) => {
       const trail = state.trail.findIndex((t) => t.x === cell.x && t.y === cell.y);
 
-      
+
   }
 
   let grid : GridCell[] = Array.from( {length: state.gridSize * state.gridSize}, (_, i) => {
@@ -26,15 +26,19 @@ const GameScreen = () => {
   })
 
   return (
-    <FlatList
-      data={grid}
-      renderItem={({item}) => (
-        <Pressable onPress={() => {}}>
-          <Text style={{fontSize:24}}> |{item.cell.x}, {item.cell.y}| </Text>
-        </Pressable>
-      )}
-      keyExtractor={item => ((item.cell.x * 7) * item.cell.y).toString()}
-      numColumns={state.gridSize} />
+    <View style={styles.container}>
+      <View>
+        <FlatList
+          data={grid}
+          renderItem={({item}) => (
+            <Pressable style={[styles.cell, { width: 50, height: 50 }]} onPress={() => {}}>
+              {/* <Text style={{fontSize:24}}> |{item.cell.x}, {item.cell.y}| </Text> */}
+            </Pressable>
+          )}
+          keyExtractor={item => ((item.cell.x * 7) * item.cell.y).toString()}
+          numColumns={state.gridSize} />
+      </View>
+    </View>
   )
 }
 
@@ -45,9 +49,17 @@ const styles = StyleSheet.create({
       width:  size,
       height:  size,
     },
-    item: {
-      flex: 1,
-      margin: 3,
-      backgroundColor: 'lightblue',
-    }
+    container: {
+        backgroundColor: '#e1f5fe',
+        alignItems: 'center',
+        paddingTop: 50,
+    },
+    cell: {
+        borderWidth: 1,
+        borderColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+        // margin: 5,
+        backgroundColor: '#004261ff'
+    },
   });
